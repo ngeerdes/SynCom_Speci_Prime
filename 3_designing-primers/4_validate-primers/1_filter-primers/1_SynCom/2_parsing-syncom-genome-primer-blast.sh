ecaa570a16d456cd94f2085d648216d8cd6f3e38
Nicole <143015121+ngeerdes@users.noreply.github.com>
2025-10-20 15:02:36 -0500

    update 2_parsing.sh

--- 3_designing-primers/4_validate-primers/1_filter-primers/1_SynCom/2_parsing-syncom-genome-primer-blast.sh
+++ 3_designing-primers/4_validate-primers/1_filter-primers/1_SynCom/2_parsing-syncom-genome-primer-blast.sh
@@ -3,7 +3,7 @@ isolates=("PvR061" "PvR090" "PvR096" "PvR101" "PvR102" "PvR112" "PvR115")
 
 # Loop through each isolate 
 for isolate in "${isolates[@]}"; do
-    python parsing-syncom13-primer-blast.py \
-        "${isolate}-parsed-primers.fasta.renamed.syncom13.blast.out" 
+    python parsing-syncom-primer-blast.py \
+        "${isolate}.syncom.blast.out" 
 done
 
