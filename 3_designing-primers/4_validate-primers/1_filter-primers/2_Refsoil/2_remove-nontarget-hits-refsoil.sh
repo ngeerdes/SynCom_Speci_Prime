10b46677156f4aa4aa733facc33883b8fb0d7c0f
Nicole <143015121+ngeerdes@users.noreply.github.com>
2025-10-21 09:16:16 -0500

    updated isolates and paths in shell script

--- 3_designing-primers/4_validate-primers/1_filter-primers/2_Refsoil/2_remove-nontarget-hits-refsoil.sh
+++ 3_designing-primers/4_validate-primers/1_filter-primers/2_Refsoil/2_remove-nontarget-hits-refsoil.sh
@@ -1,15 +1,13 @@
+
+
 # Define an array of isolate IDs
-isolates=("PvR061" "PvR090" "PvR096" "PvR101" "PvR102" "PvR112" "PvR115")
+isolates=("PvR021" "PvR079" "PvR083" "PvR122" "PvR147") 
 
 # Loop through each isolate and execute the Python script
-for isolate in "${isolates[@]}"; do
-    python remove-nontarget-hits.py \
-        "${isolate}-parsed-primers.fasta.renamed.syncom13.refsoil.blast.out" \
-        "../primer3-filtered/${isolate}-parsed-primers.fasta.renamed.targethits.filtered.syncom13" \
-        "../primer3-filtered/${isolate}-parsed-primers.fasta.renamed.targethits.filtered.syncom13.refsoil"
+for isolates in "${isolates[@]}"; do
+    python ../remove-nontarget-hits.py \
+        "../../validation/refsoil/blast-out/${isolates}.refsoil.blast.out" \
+        "../../validation/syncom/filtered/${isolates}.filtered.syncom.primers.fasta" \
+        "../../validation/refsoil/filtered/${isolates}.filtered.syncom.refsoil.primers.fasta"
 done
 
-
-
-
-## python remove-nontarget-hits.py PvR045-parsed-primers.fasta.renamed.filtered.syncom13.refsoil.blast.out ../primer3-filtered/PvR045-parsed-primers.fasta.renamed.targethits.filtered.syncom13 ../primer3-filtered/PvR045-parsed-primers.fasta.renamed.targethits.filtered.syncom13.refsoil
