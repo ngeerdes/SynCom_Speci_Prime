9d8ae81620c89176a3d96133ec8c8fc1cba3cc5d
Nicole <143015121+ngeerdes@users.noreply.github.com>
2025-10-21 09:44:28 -0500

    updated python script called

--- 3_designing-primers/3_format-primers/1_parse-primer3-out.sh
+++ 3_designing-primers/3_format-primers/1_parse-primer3-out.sh
@@ -4,7 +4,7 @@ isolates=("PvR021" "PvR079" "PvR083" "PvR122" "PvR147")
 
 # Loop through each isolate and execute the Python script 
 for isolates in "${isolates[@]}"; do
-    python ../primer3-out-to-fasta.py \
+    python ../primer3-probe-to-fasta.py \
         ../../primers/primer3-out/"${isolates}.txt" \
         ../../primers/primers-fasta/"${isolates}.primers.fasta"
 done
